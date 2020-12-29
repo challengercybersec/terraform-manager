f7f2e168a1e1b0a835c6b473149e3227e5c6ee45
Challenger <challengercybersec@gmail.com>
2020-12-29 09:28:58 +0100

    Vnet Creation

--- manager.tf
+++ manager.tf
@@ -11,3 +11,23 @@ resource "azurerm_resource_group" "RG-ManagerMDS" {
   name     = var.rsgname
   location = var.region
 }
+
+
+
+#Vnet Subnet Config
+
+resource "azurerm_virtual_network" "Vnet-Manager" {
+  name                = "vnet-manager"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.RG-ManagerMDS.name
+  address_space       = ["10.4.20.0/24"]
+
+  subnet {
+    name           = "subnet-manager"
+    address_prefixes = ["10.4.20.0/25"]
+  }
+
+  tags = {
+    owner = "Terraform Automation"
+  }
+}
